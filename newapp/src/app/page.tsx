3daf4efdfb2319c69af15460fcc292480aec920c
kartik <kartikdev2244@gamil.com>
2024-01-05 23:43:04 +0530

    first commit

---
+++ newapp/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad laboriosam, nihil sapiente deserunt, rem veritatis obcaecati aperiam natus, odit porro unde harum deleniti vitae! Odio enim provident assumenda sed ad!</p>
+    </main>
+  )
+}
